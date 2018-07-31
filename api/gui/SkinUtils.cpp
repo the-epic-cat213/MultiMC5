1138bf642731228c384c8959da3496dd488e2e75
the-epic-cat213 <eaglekai34@hotmail.com>
2018-07-30 19:57:59 -0500

    Update SkinUtils.cpp

--- api/gui/SkinUtils.cpp
+++ api/gui/SkinUtils.cpp
@@ -1,4 +1,5 @@
-/* Copyright 2013-2018 MultiMC Contributors
+/* Not Copyrighted, modified by the_epic_cat213
+ * Original Project Copyright 2013-2018 MultiMC Contributors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
